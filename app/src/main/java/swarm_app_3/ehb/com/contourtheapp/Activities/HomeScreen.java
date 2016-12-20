894043976e0d0d7f4c868f7fb634dd5bbc6c98df
BrentJanssens <brent.janssens@student.ehb.be>
2016-12-20 15:33:24 +0100

    Video enzo voor homepagina

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/HomeScreen.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/HomeScreen.java
@@ -2,13 +2,19 @@ package swarm_app_3.ehb.com.contourtheapp.Activities;
 
 
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
 
 public class HomeScreen extends AppCompatActivity {
+
+    VideoView videov;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,23 @@ public class HomeScreen extends AppCompatActivity {
         Typeface customFonts = Typeface.createFromAsset(this.getAssets(), "Raleway-Thin.ttf");
         contourtitel.setTypeface(customFonts);
         ctitel.setTypeface(customFonts);
-    }
+        videov = (VideoView) findViewById(R.id.VidV);
+        videoplay(videov);
+        videov.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+                videov.start();
+
+            }
+        });
+
+    }
+public void videoplay(View v){
+    String path = "android.resource://" + getPackageName() + "/" + R.raw.lines;
+    Uri uri = Uri.parse(path);
+    videov.setVideoURI(uri);
+    videov.start();
+}
 }
