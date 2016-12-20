499ea5f0f349b8970a811707990d8359d9e9ffff
Sam <sam.goeman@hotmail.com>
2016-12-20 18:19:24 +0100

    Splashscreen delay

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/SplashScreen.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/SplashScreen.java
@@ -1,5 +1,6 @@
 package swarm_app_3.ehb.com.contourtheapp.Activities;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,5 +19,20 @@ public class SplashScreen extends AppCompatActivity {
         Typeface customFonts = Typeface.createFromAsset(this.getAssets(), "Raleway-Thin.ttf");
         contourtitel.setTypeface(customFonts);
         ctitel.setTypeface(customFonts);
+        Thread SplashThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent splashToMain = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(splashToMain);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        SplashThread.start();
     }
 }
