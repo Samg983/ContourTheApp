c9e3579024a34953ecfad1ca96234a1d02d83dcf
BramSchrooyen <bram.schrooyen@student.ehb.be>
2016-12-20 12:40:36 +0100

    Menu toevoegen

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MainActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton.LayoutParams actionButtonParams = new FloatingActionButton.LayoutParams(300,300);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createMenu();
     }
 
     public void goToSplash(View view) {
@@ -27,4 +37,88 @@ public class MainActivity extends AppCompatActivity {
         Intent toHome = new Intent(this, HomeScreen.class);
         startActivity(toHome);
     }
+
+
+    public void createMenu(){
+        ImageView icon = new ImageView(this); // Create an icon
+        icon.setImageDrawable(getDrawable(R.drawable.circle_menu_green));
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(icon)
+                .setPosition(5)
+                .setLayoutParams(actionButtonParams)
+                .build();
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        // repeat many times:
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageDrawable(getDrawable(R.drawable.menu_item1));
+        SubActionButton swarmButton = itemBuilder.setContentView(itemIcon).setLayoutParams(new FloatingActionButton.LayoutParams(450, 225)).setBackgroundDrawable(getDrawable(R.drawable.transparent)).build();
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageDrawable(getDrawable(R.drawable.menu_item2));
+        SubActionButton blogButton = itemBuilder.setContentView(itemIcon2).build();
+
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageDrawable(getDrawable(R.drawable.transparent));
+        SubActionButton button3 = itemBuilder.setContentView(itemIcon3).build();
+
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageDrawable(getDrawable(R.drawable.menu_item3));
+        SubActionButton trackerButton = itemBuilder.setContentView(itemIcon4).build();
+
+        ImageView itemIcon5 = new ImageView(this);
+        itemIcon5.setImageDrawable(getDrawable(R.drawable.menu_item4));
+        SubActionButton galleryButton = itemBuilder.setContentView(itemIcon5).build();
+
+        final FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(swarmButton)
+                .addSubActionView(blogButton)
+                .addSubActionView(button3)
+                .addSubActionView(trackerButton)
+                .addSubActionView(galleryButton)
+                .setStartAngle(180)
+                .setEndAngle(360)
+                .setRadius(350)
+                // ...
+                .attachTo(actionButton)
+                .build();
+
+        swarmButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent toMaps = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        blogButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent toMaps = new Intent(MainActivity.this, SplashScreen.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        trackerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent toMaps = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent toMaps = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+    }
+
 }
