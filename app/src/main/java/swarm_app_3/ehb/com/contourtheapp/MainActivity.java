ee4e4454f6b2e378f349b2bc7d277bd3368cf106
Sam <sam.goeman@hotmail.com>
2016-12-20 10:52:20 +0100

    test

---
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MainActivity.java
@@ -0,0 +1,14 @@
+package swarm_app_3.ehb.com.contourtheapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //test
+    }
+}
