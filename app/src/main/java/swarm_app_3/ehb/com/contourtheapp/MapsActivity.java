74787f7a2a7864b02679ad8abcca361d359fa961
Sam <sam.goeman@hotmail.com>
2016-12-20 11:46:24 +0100

    Maps Activity

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MapsActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MapsActivity.java
@@ -1,18 +1,28 @@
 package swarm_app_3.ehb.com.contourtheapp;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private LatLngBounds EUROPE = new LatLngBounds(
+            new LatLng(48.6908333333, 9.14055555556), new LatLng(48.6908333333, 9.14055555556));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +48,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Gent = new LatLng(51.0535, 3.7304);
+        LatLng Biella = new LatLng(45.5630400, 8.0579600);
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(Gent, Biella)
+                .width(5)
+                .color(Color.GREEN));
+
+        mMap.setMinZoomPreference(6.0f);
+        mMap.setMaxZoomPreference(14.0f);
+
+
+
+// Set the camera to the greatest possible zoom level that includes the
+// bounds
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(EUROPE, 0));
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+
+        }
     }
 }
