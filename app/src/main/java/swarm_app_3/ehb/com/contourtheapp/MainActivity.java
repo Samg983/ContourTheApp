5074e32d6761234d361f4c7fe2893397fdc59d90
BrentJanssens <brent.janssens@student.ehb.be>
2016-12-20 11:32:29 +0100

    Test
    Test

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MainActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/MainActivity.java
@@ -1,7 +1,9 @@
 package swarm_app_3.ehb.com.contourtheapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSplash(View view) {
+        Intent toSplash = new Intent(this, SplashScreen.class);
+        startActivity(toSplash);
     }
 }
