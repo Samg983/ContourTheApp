3c87111fdeaf2169007f1392931e6e9ecd185a65
Sam <sam.goeman@hotmail.com>
2016-12-21 15:37:20 +0100

    maps activity

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/MapsActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/MapsActivity.java
@@ -13,8 +13,11 @@ import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,16 +30,23 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 import swarm_app_3.ehb.com.contourtheapp.Model.OpenDialog;
 import swarm_app_3.ehb.com.contourtheapp.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    FloatingActionButton.LayoutParams actionButtonParams;
+    FloatingActionButton.LayoutParams subActionButtonParams;
+    int radius;
     private GoogleMap mMap;
     private static final String TAG = MapsActivity.class.getSimpleName();
-    private LatLngBounds EUROPE = new LatLngBounds(
-            new LatLng(41.390205, 2.154007), new LatLng(55.676098, 12.568337));
+   
+    boolean alreadySubscribed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         TextView next = (TextView) findViewById(R.id.lblNext);
         Typeface customFonts = Typeface.createFromAsset(this.getAssets(), "Raleway-Thin.ttf");
         next.setTypeface(customFonts);
+
+        Button subscribeButton = (Button) findViewById(R.id.btnSubscribe1);
+
+
+        if(alreadySubscribed){
+            subscribeButton.setVisibility(View.INVISIBLE);
+            next.setVisibility(View.INVISIBLE);
+            createMenu();
+        } else {
+            subscribeButton.setVisibility(View.VISIBLE);
+        }
     }
 
 
@@ -68,17 +89,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng Gent = new LatLng(51.0535, 3.7304);
         LatLng Biella = new LatLng(45.5630400, 8.0579600);
-        LatLng België = new LatLng(50.503887,4.4699359999999615);
+        LatLng Strasbourg = new LatLng(48.58392,7.74553);
+        LatLng Paris = new LatLng(48.864716,2.349014);
 
         Polyline line = mMap.addPolyline(new PolylineOptions()
                 .add(Gent, Biella)
                 .width(5)
-                .color(Color.GREEN));
+                .color(Color.BLACK));
+
+        Polyline line2 = mMap.addPolyline(new PolylineOptions()
+                .add(Paris, Biella)
+                .width(5)
+                .color(Color.BLACK));
 
         mMap.setMinZoomPreference(6.0f);
-        mMap.setMaxZoomPreference(42.0f);
+        mMap.setMaxZoomPreference(100.0f);
 
-       mMap.moveCamera(CameraUpdateFactory.newLatLng(België));
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(Strasbourg));
         try {
             boolean success = googleMap.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
@@ -106,5 +133,115 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void goToHomeFromSwarm(View view) {
         Intent toHomeFromSwarm = new Intent(this, HomeScreen.class);
         startActivity(toHomeFromSwarm);
+    }
+    public void createMenu() {
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        switch (metrics.densityDpi) {
+            case DisplayMetrics.DENSITY_HIGH:
+            case DisplayMetrics.DENSITY_XHIGH:
+            case DisplayMetrics.DENSITY_XXHIGH:
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                actionButtonParams = new FloatingActionButton.LayoutParams(300, 300);
+                subActionButtonParams = new FloatingActionButton.LayoutParams(450, 225);
+                radius = 350;
+                break;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                actionButtonParams = new FloatingActionButton.LayoutParams(200, 200);
+                subActionButtonParams = new FloatingActionButton.LayoutParams(250, 125);
+                radius = 225;
+                break;
+            case DisplayMetrics.DENSITY_LOW:
+                actionButtonParams = new FloatingActionButton.LayoutParams(100, 100);
+                subActionButtonParams = new FloatingActionButton.LayoutParams(150, 75);
+                radius = 125;
+                break;
+            default:
+                actionButtonParams = new FloatingActionButton.LayoutParams(300, 300);
+                subActionButtonParams = new FloatingActionButton.LayoutParams(450, 225);
+                radius = 350;
+        }
+
+        ImageView icon = new ImageView(this); // Create an icon
+        icon.setImageDrawable(getDrawable(R.drawable.circle_menu_green));
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(icon)
+                .setPosition(5)
+                .setLayoutParams(actionButtonParams)
+                .build();
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        // repeat many times:
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageDrawable(getDrawable(R.drawable.menu_item1));
+        SubActionButton swarmButton = itemBuilder.setContentView(itemIcon).setLayoutParams(subActionButtonParams).setBackgroundDrawable(getDrawable(R.drawable.transparent)).build();
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageDrawable(getDrawable(R.drawable.menu_item2));
+        SubActionButton blogButton = itemBuilder.setContentView(itemIcon2).build();
+
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageDrawable(getDrawable(R.drawable.transparent));
+        SubActionButton button3 = itemBuilder.setContentView(itemIcon3).build();
+
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageDrawable(getDrawable(R.drawable.menu_item3));
+        SubActionButton trackerButton = itemBuilder.setContentView(itemIcon4).build();
+
+        ImageView itemIcon5 = new ImageView(this);
+        itemIcon5.setImageDrawable(getDrawable(R.drawable.menu_item4));
+        SubActionButton galleryButton = itemBuilder.setContentView(itemIcon5).build();
+
+        final FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(swarmButton)
+                .addSubActionView(blogButton)
+                .addSubActionView(button3)
+                .addSubActionView(trackerButton)
+                .addSubActionView(galleryButton)
+                .setStartAngle(180)
+                .setEndAngle(360)
+                .setRadius(radius)
+                // ...
+                .attachTo(actionButton)
+                .build();
+
+        swarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toMaps = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        blogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toMaps = new Intent(getApplicationContext(), SplashScreen.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        trackerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toMaps = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toMaps = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(toMaps);
+                actionMenu.close(true);
+            }
+        });
+
     }
 }
