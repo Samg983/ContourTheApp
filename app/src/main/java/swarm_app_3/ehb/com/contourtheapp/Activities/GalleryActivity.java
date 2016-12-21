fd0eca0849e3050fcbba5febc0453cf1fa3b2cc7
BramSchrooyen <bram.schrooyen@student.ehb.be>
2016-12-21 10:11:17 +0100

    start gallery

---
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/GalleryActivity.java
@@ -0,0 +1,15 @@
+package swarm_app_3.ehb.com.contourtheapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import swarm_app_3.ehb.com.contourtheapp.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
