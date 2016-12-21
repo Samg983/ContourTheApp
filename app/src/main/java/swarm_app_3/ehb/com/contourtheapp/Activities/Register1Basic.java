dbb8ef52e9868d580df391f9e46a5abce69d1684
BrentJanssens <brent.janssens@student.ehb.be>
2016-12-21 13:36:46 +0100

    qrsdf

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register1Basic.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register1Basic.java
@@ -3,8 +3,6 @@ package swarm_app_3.ehb.com.contourtheapp.Activities;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
@@ -17,7 +15,7 @@ public class Register1Basic extends AppCompatActivity {
         setContentView(R.layout.activity_register1_basic);
         TextView titel = (TextView) findViewById(R.id.titel);
         TextView name = (TextView) findViewById(R.id.name);
-        TextView age = (TextView) findViewById(R.id.age);
+        TextView age = (TextView) findViewById(R.id.dateofbirth);
         TextView city = (TextView) findViewById(R.id.city);
         Typeface customFonts = Typeface.createFromAsset(this.getAssets(), "CutiveMono-Regular.ttf");
         titel.setTypeface(customFonts);
