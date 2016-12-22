625054668017614a4cefc0a4dbd69c34d4464518
BrentJanssens <brent.janssens@student.ehb.be>
2016-12-22 12:22:44 +0100

    registratie 3 & 4 + changes

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register3Weird.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register3Weird.java
@@ -1,7 +1,9 @@
 package swarm_app_3.ehb.com.contourtheapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
 
@@ -11,5 +13,15 @@ public class Register3Weird extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register3_weird);
+    }
+
+    public void goToRegi2 (View view) {
+        Intent toRegi2 = new Intent(this, Register2Personal.class);
+        startActivity(toRegi2);
+    }
+
+    public void goToRegi4 (View view) {
+        Intent toRegi4 = new Intent(this, Register4AlmostFinished.class);
+        startActivity(toRegi4);
     }
 }
--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register4AlmostFinished.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register4AlmostFinished.java
@@ -1,7 +1,9 @@
 package swarm_app_3.ehb.com.contourtheapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
 
@@ -11,5 +13,9 @@ public class Register4AlmostFinished extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register4_almost_finished);
+    }
+    public void goToRegi3 (View view) {
+        Intent toRegi3 = new Intent(this, Register3Weird.class);
+        startActivity(toRegi3);
     }
 }
