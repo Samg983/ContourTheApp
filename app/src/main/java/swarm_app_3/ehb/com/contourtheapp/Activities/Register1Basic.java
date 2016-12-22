3295ebf734bfa7212775f7bebac1de200e4f04ad
purraxxus <thatcronosdude@outlook.com>
2016-12-22 10:49:44 +0100

    placeAutocomplete start

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register1Basic.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/Register1Basic.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
 
