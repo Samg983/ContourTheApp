644f1a81f110b856dbc6728bff8056c7b5bc8bbe
BrentJanssens <brent.janssens@student.ehb.be>
2016-12-24 12:51:30 +0100

    Iets voor locatie in manifest toegevoegd, zorgen dat je niet kan in- of uitzoomen + arrow toegevoegd

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/TrackerActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/TrackerActivity.java
@@ -1,30 +1,39 @@
 package swarm_app_3.ehb.com.contourtheapp.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
-import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import swarm_app_3.ehb.com.contourtheapp.R;
 
 public class TrackerActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
     private GoogleMap mMap;
     private static final String TAG = TrackerActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -40,25 +49,50 @@ public class TrackerActivity extends FragmentActivity implements OnMapReadyCallb
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+
+        } else{
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            LatLng myCurrentLocation = new LatLng(50.8424, 4.32254);
+            Marker myLocation = mMap.addMarker(new MarkerOptions()
+                    .position(myCurrentLocation)
+                    .title("Your current location")
+                    .anchor(0.5f,0.5f)
+                    .zIndex(9999)
+                    .snippet("Send messages to other people!")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow)));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(20);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myCurrentLocation));
+            mMap.animateCamera(zoom);
 
-        try {
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.map_style_json));
 
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.");
+
+
+            try {
+                boolean success = googleMap.setMapStyle(
+                        MapStyleOptions.loadRawResourceStyle(
+                                this, R.raw.map_style_json));
+                mMap.setMyLocationEnabled(true);
+
+
+                if (!success) {
+                    Log.e(TAG, "Style parsing failed.");
+                }
+            } catch (Resources.NotFoundException e) {
+
             }
-        } catch (Resources.NotFoundException e) {
-
         }
     }
 }
