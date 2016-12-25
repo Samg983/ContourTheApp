ea8833d231879e1df60dc1ddea867ad21190b6ca
Sam <sam.goeman@hotmail.com>
2016-12-25 13:37:20 +0100

    Current location works + green arrow, still blue dot :(

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/TrackerActivity.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Activities/TrackerActivity.java
@@ -142,7 +142,7 @@ public class TrackerActivity extends FragmentActivity implements OnMapReadyCallb
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow));
         mCurrLocationMarker = mMap.addMarker(markerOptions);
 
         //move map camera
