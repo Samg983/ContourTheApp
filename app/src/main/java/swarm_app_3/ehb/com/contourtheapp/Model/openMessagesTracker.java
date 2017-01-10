37e5f6b3d3ba006ccf36bbcf5801500f2843789a
Sam <sam.goeman@hotmail.com>
2017-01-10 15:57:48 +0100

    Photo done

--- app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Model/openMessagesTracker.java
+++ app/src/main/java/swarm_app_3/ehb/com/contourtheapp/Model/openMessagesTracker.java
@@ -2,15 +2,26 @@ package swarm_app_3.ehb.com.contourtheapp.Model;
 
         import android.app.Activity;
         import android.app.Dialog;
+        import android.content.ContentResolver;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
         import android.graphics.Typeface;
+        import android.net.Uri;
         import android.os.Bundle;
+        import android.os.Environment;
+        import android.provider.MediaStore;
         import android.view.View;
         import android.view.Window;
         import android.widget.Button;
+        import android.widget.ImageView;
         import android.widget.TextView;
+
+        import java.io.File;
 
         import swarm_app_3.ehb.com.contourtheapp.Activities.TrackerActivity;
         import swarm_app_3.ehb.com.contourtheapp.R;
+
+        import static com.google.android.gms.instantapps.InstantApps.getPackageManager;
 
 /**
  * Created by SamGoeman on 25/12/2016.
@@ -18,7 +29,9 @@ package swarm_app_3.ehb.com.contourtheapp.Model;
 
 public class OpenMessagesTracker extends Dialog implements android.view.View.OnClickListener {
     public Activity activity;
-    public TextView lblMessage, lblMoveAlong, lblHiCutie, lblWalkOnTheLine;
+    public TextView lblMessage, lblMoveAlong, lblHiCutie, lblWalkOnTheLine, lblTakePhoto;
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
 
     public OpenMessagesTracker() {
         super(null);
@@ -42,11 +55,26 @@ public class OpenMessagesTracker extends Dialog implements android.view.View.OnC
         lblMoveAlong = (TextView) findViewById(R.id.lblMoveAlong);
         lblHiCutie = (TextView) findViewById(R.id.lblHi);
         lblWalkOnTheLine = (TextView) findViewById(R.id.lblWalkOnTheLine);
+        lblTakePhoto = (TextView) findViewById(R.id.lblTakePhoto);
 
         lblMessage.setTypeface(ralewayRegular);
         lblMoveAlong.setTypeface(cutive);
         lblHiCutie.setTypeface(cutive);
         lblWalkOnTheLine.setTypeface(cutive);
+        lblTakePhoto.setTypeface(cutive);
+
+        lblTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photo));
+                imageUri = Uri.fromFile(photo);
+                activity.startActivityForResult(intent, TAKE_PICTURE);
+
+            }
+        });
 
 
     }
